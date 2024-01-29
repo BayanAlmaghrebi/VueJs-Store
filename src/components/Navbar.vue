6303e9225a2a2e33388c80bbc29f9d0cd375ca34
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2024-01-29 16:24:25 +0100

    add Views & Components Structure

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3> Navbar </h3>
+</template>
+
+<script>
+    export default{
+        name: 'Navbar'
+    }
+</script>
---
+++ src/views/CourseDetailView.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3> Courses Detail </h3>
+</template>
+
+<script>
+    export default{
+        name: 'CourseDetail'
+    }
+</script>
