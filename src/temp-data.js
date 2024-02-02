5b317524bf231a72b5479a62936337c87969faa7
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2024-02-02 20:35:50 +0100

    show courses from temp-data.js

---
+++ src/temp-data.js
@@ -0,0 +1,35 @@
+import img1 from './assets/1.png'
+import img2 from './assets/2.png'
+import img3 from './assets/3.jpg'
+import img4 from './assets/4.jpg'
+
+export const courses = [
+    {
+        id:1 ,
+        name: 'Python Basics' ,
+        Category: 'Web Development',
+        price: 30 , 
+        image: img1
+    },
+    {
+        id:2 , 
+        name: 'AI Basics' , 
+        Category: 'AI Development',
+        price: 40 , 
+        image: img2
+    },
+    {
+        id:3 , 
+        name: 'Flutter Basics' , 
+        Category: 'Mobile Development',
+        price: 50 , 
+        image: img3
+    },
+    {
+        id:4 , 
+        name: 'Django Basics' , 
+        Category: 'Web Development',
+        price: 60 , 
+        image: img4
+    },
+]
