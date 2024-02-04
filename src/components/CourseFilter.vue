d5415b1af628a566d5ffb7ecc690c70431799fd5
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2024-02-04 23:58:44 +0100

    add PrimeVue theme & filter panel

--- src/components/CourseFilter.vue
+++ src/components/CourseFilter.vue
@@ -1,9 +1,35 @@
 <template>
-    <h2> Courses Filter </h2>
+    <h3> Courses Filter </h3>
+    <form class="card ">
+
+        <div class="flex align-items-center mycheckbox" v-for="cat in category" :key="cat.id">
+            <Checkbox v-model="selectedCategory" :inputId="cat.id" :name="cat.name" :value="cat.id" />
+            <label :for="cat.id" class="ml-2"> {{cat.name}} </label>
+        </div>
+
+    </form>
 </template>
 
 <script>
     export default{
-        name: 'CourseFilter'
+        name: 'CourseFilter',
+        data(){
+            return {
+                category : [
+                    {id:1 , name:"Web Development"},
+                    {id:2 , name:'Mobile Development'},
+                    {id:3 , name:'AI Development'},
+                    {id:4 , name:'Game Development'},
+                    {id:5 , name:'Deep Learning'}
+                ],
+                selectedCategory: []
+            }
+        }
     }
 </script>
+
+<style>
+    .mycheckbox{
+        margin:10px 0px;
+    }
+</style>
